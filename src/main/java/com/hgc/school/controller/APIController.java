f244ab19c3ef67c10912cba1ee4e90464b4a858d
15zed <108381807+15zed@users.noreply.github.com>
2024-06-03 19:43:18 +0800

    Update APIController.java
    使用Guava-Retrying，为部分接口添加容错机制

--- src/main/java/com/hgc/school/controller/APIController.java
+++ src/main/java/com/hgc/school/controller/APIController.java
@@ -2,6 +2,7 @@ package com.hgc.school.controller;
 
 import com.alibaba.fastjson.JSON;
 
+import com.github.rholder.retry.*;
 import com.hgc.school.commons.ErrorCode;
 import com.hgc.school.dto.InfoWithCommentsDTO;
 import com.hgc.school.exception.BusinessException;
@@ -22,7 +23,9 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -69,6 +72,7 @@ public class APIController {
 
     /**
      * 添加关注
+     * 使用事务
      *
      * @param userId  被关注人
      * @param session 获取关注人
@@ -78,13 +82,14 @@ public class APIController {
     public String follow(@PathVariable Integer userId, HttpSession session) {
         if (userId == null || userId.equals(0)) throw new BusinessException(ErrorCode.PARAMS_ERROR);
         String jsonUser = (String) session.getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
         return userService.follow(user, userId, session);
     }
 
     /**
      * 取消关注
+     * 使用事务
      *
      * @param userId  取消关注谁
      * @param session 获取取消人
@@ -94,7 +99,7 @@ public class APIController {
     public String unfollow(@PathVariable Integer userId, HttpSession session) {
         if (userId == null || userId.equals(0)) throw new BusinessException(ErrorCode.PARAMS_ERROR);
         String jsonUser = (String) session.getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
         return userService.cancel(user, userId, session);
     }
@@ -111,7 +116,7 @@ public class APIController {
     public ModelAndView getFollows(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
         String jsonUser = (String) session.getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
         List<User> followList = userService.selectFollows(user.getUserId());
         modelAndView.addObject("followList", followList);
@@ -131,7 +136,7 @@ public class APIController {
     public ModelAndView getFans(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
         String jsonUser = (String) session.getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
         List<User> fansList = userService.selectFans(user.getUserId());
         User newUser = userService.selectById(user.getUserId());
@@ -163,10 +168,29 @@ public class APIController {
     @PostMapping("/post")
     public String addInfo(@RequestBody Info info) throws ExecutionException, InterruptedException {
         if (info == null) throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        String uniqueId = infoService.generateUniqueId(info);
+        if (!redisService.setUniqueId(uniqueId)) {
+            return null;
+        }
         //这里设置了会把生成的主键id赋值给info对象的id字段 返回info的所有信息 不然前端的有些操作会拿不到帖子的id
-        Integer mysqlFlag = infoService.add(info).get();
-        Integer esFlag = esService.add(ESIndexUtil.INFO_INDEX, info).get();
-        flagService.addFlag(new Flag(info.getId(), mysqlFlag, 1, esFlag, 0, 1));
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(result -> result)//如果结果为true，重试，这里没有设置暂停策略，所以会一直重试结果为false
+                .withWaitStrategy(WaitStrategies.fixedWait(5L, TimeUnit.SECONDS))
+                .build();
+        Flag flag = new Flag();//这个对象是为了在lambda表达式中绕过对变量必须为final的要求
+        Callable<Boolean> retryTask = () -> {
+            flag.mysqlFlag = infoService.add(info).get();
+            flag.esFlag = esService.add(ESIndexUtil.INFO_INDEX, info).get();
+            //// 只要其中一个成功（非0），就返回false结束重试
+            return flag.mysqlFlag == 0 && flag.esFlag == 0;
+        };
+        try {
+            // //如果MySQL和ES都失败，那么重新操作，直到其中一个成功，不需要全部都成功，因为采用的是弱一致方案，有定时任务补偿
+            retryer.call(retryTask);
+            flagService.addFlag(new Flag(info.getId(), flag.mysqlFlag, 1, flag.esFlag, 0, 1));
+        } catch (RetryException e) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
         return JSON.toJSONString(info);
     }
 
@@ -179,18 +203,36 @@ public class APIController {
     @PostMapping("/comment")
     public String addComment(@RequestBody CommentInfo comment) throws ExecutionException, InterruptedException {
         if (comment == null) throw new BusinessException(ErrorCode.PARAMS_ERROR);
-        //设置主键生成之后自动往commentInfo对象的commentId字段加
-        Integer addFlag1 = commentService.add(comment).get();
-        Integer addFlag2 = infoService.updateComments(comment.getPubId()).get();
-        Integer mysqlFlag = (addFlag1 & addFlag2);
+        String uniqueId = commentService.generateUniqueId(comment);
+        if (!redisService.setUniqueId(uniqueId)) {
+            return null;
+        }
         //更新ES
         Integer comments = infoService.selectById(comment.getPubId()).getComments();
         Map<String, Object> doc = new HashMap<>();
         doc.put("comments", comments);
-        Integer esFlag1 = esService.add(ESIndexUtil.COMMENT_INDEX, comment).get();
-        Integer esFlag2 = esService.update(ESIndexUtil.INFO_INDEX, String.valueOf(comment.getPubId()), doc).get();
-        Integer esFlag = (esFlag1 & esFlag2);
-        flagService.addFlag(new Flag(comment.getCommentId(), mysqlFlag, 1, esFlag, 0, 2));
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(result -> result)//如果结果为true，重试，这里没有设置暂停策略，所以会一直重试结果为false
+                .withWaitStrategy(WaitStrategies.fixedWait(5L, TimeUnit.SECONDS))
+                .build();
+        Flag flag = new Flag();//这个对象是为了在lambda表达式中绕过对变量必须为final的要求
+        Callable<Boolean> retryTask = () -> {
+            Integer addFlag1 = commentService.add(comment).get();
+            Integer addFlag2 = infoService.updateComments(comment.getPubId()).get();
+            Integer esFlag1 = esService.add(ESIndexUtil.COMMENT_INDEX, comment).get();
+            Integer esFlag2 = esService.update(ESIndexUtil.INFO_INDEX, String.valueOf(comment.getPubId()), doc).get();
+            flag.mysqlFlag = (addFlag1 & addFlag2);
+            flag.esFlag = (esFlag1 & esFlag2);
+            //// 只要其中一个成功（非0），就返回false结束重试
+            return flag.mysqlFlag == 0 && flag.esFlag == 0;
+        };
+        try {
+            // //如果MySQL和ES都失败，那么重新操作，直到其中一个成功，不需要全部都成功，因为采用的是弱一致方案，有定时任务补偿
+            retryer.call(retryTask);
+            flagService.addFlag(new Flag(comment.getCommentId(), flag.mysqlFlag, 1, flag.esFlag, 0, 2));
+        } catch (RetryException e) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
         return JSON.toJSONString(comment);
     }
 
@@ -206,18 +248,10 @@ public class APIController {
     public ResponseEntity<?> addLikes(@PathVariable("id") Integer id, HttpServletRequest request) throws ExecutionException, InterruptedException {
         if (id == null || id.equals(0)) throw new BusinessException(ErrorCode.PARAMS_ERROR);
         String jsonUser = (String) request.getSession().getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
-        // 检查用户是否已经点赞过该帖子
-        if (userService.hasUserLikedPost(user.getUserId(), id)) {
-            return ResponseEntity
-                    .badRequest()
-                    .body("用户已经点赞过该帖子。");
-        }
         // 更新mysql:用户表添加点赞列表，帖子表添加点赞数
-        Integer flag1 = userService.addPostToUserLikes(user.getUserId(), id).get();
-        Integer flag2 = infoService.updateById(id).get();
-        Integer mysqlFlag = (flag1 & flag2);
+
         //删除redis
         Integer redisFlag = redisService.delete(user.getUserId()).get();
         //更新ES
@@ -227,8 +261,75 @@ public class APIController {
         // 构建部分更新的文档
         Map<String, Object> doc = new HashMap<>();
         doc.put("likes", likes);
-        Integer esFlag = esService.update(ESIndexUtil.INFO_INDEX, String.valueOf(id), doc).get();
-        flagService.addFlag(new Flag(id, mysqlFlag, redisFlag, esFlag, user.getUserId(), 3));
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(result -> result)//如果结果为true，重试，这里没有设置暂停策略，所以会一直重试结果为false
+                .withWaitStrategy(WaitStrategies.fixedWait(5L, TimeUnit.SECONDS))
+                .build();
+        Flag flag = new Flag();//这个对象是为了在lambda表达式中绕过对变量必须为final的要求
+        Callable<Boolean> retryTask = () -> {
+            Integer flag1 = userService.addPostToUserLikes(user.getUserId(), id).get();
+            Integer flag2 = infoService.updateById(id).get();
+            flag.mysqlFlag = (flag1 & flag2);
+            flag.esFlag = esService.update(ESIndexUtil.INFO_INDEX, String.valueOf(id), doc).get();
+            // 只要其中一个成功（非0），就返回false结束重试
+            return flag.mysqlFlag == 0 && flag.esFlag == 0;
+        };
+        try {
+            //如果MySQL和ES都失败，那么重新操作，直到其中一个成功，不需要全部都成功，因为采用的是弱一致方案，有定时任务补偿
+            retryer.call(retryTask);
+            flagService.addFlag(new Flag(id, flag.mysqlFlag, redisFlag, flag.esFlag, user.getUserId(), 3));
+        } catch (RetryException e) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
+        return ResponseEntity
+                .ok()
+                .body(info);
+    }
+
+    /**
+     * 取消点赞
+     *
+     * @param id      帖子的id
+     * @param request
+     * @return
+     */
+    @PostMapping("/{id}/dislike")
+    public ResponseEntity<?> cancelLikes(@PathVariable("id") Integer id, HttpServletRequest request) throws ExecutionException, InterruptedException {
+        if (id == null || id.equals(0)) throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        String jsonUser = (String) request.getSession().getAttribute("user");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
+        User user = JSON.parseObject(jsonUser, User.class);
+        // 更新mysql:用户表取消点赞列表，帖子表减少点赞数
+
+        //删除redis
+        Integer redisFlag = redisService.delete(user.getUserId()).get();
+        //更新ES
+        // 返回更新后的帖子数据
+        Info info = infoService.selectById(id);
+        Integer likes = info.getLikes();
+        // 构建部分更新的文档
+        Map<String, Object> doc = new HashMap<>();
+        doc.put("likes", likes);
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(result -> result)//如果结果为true，重试，这里没有设置暂停策略，所以会一直重试结果为false
+                .withWaitStrategy(WaitStrategies.fixedWait(5L, TimeUnit.SECONDS))
+                .build();
+        Flag flag = new Flag();//这个对象是为了在lambda表达式中绕过对变量必须为final的要求
+        Callable<Boolean> retryTask = () -> {
+            Integer flag1 = userService.cancelPostToUserLikes(user.getUserId(), id).get();
+            Integer flag2 = infoService.decreaseUpdateById(id).get();
+            flag.mysqlFlag = (flag1 & flag2);
+            flag.esFlag = esService.update(ESIndexUtil.INFO_INDEX, String.valueOf(id), doc).get();
+            // 只要其中一个成功（非0），就返回false结束重试
+            return flag.mysqlFlag == 0 && flag.esFlag == 0;
+        };
+        try {
+            //如果MySQL和ES都失败，那么重新操作，直到其中一个成功，不需要全部都成功，因为采用的是弱一致方案，有定时任务补偿
+            retryer.call(retryTask);
+            flagService.addFlag(new Flag(id, flag.mysqlFlag, redisFlag, flag.esFlag, user.getUserId(), 3));
+        } catch (RetryException e) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
         return ResponseEntity
                 .ok()
                 .body(info);
@@ -244,11 +345,27 @@ public class APIController {
     public String deleteInfoWithComments(@PathVariable("id") Integer id, HttpServletRequest request) throws ExecutionException, InterruptedException {
         if (id == null || id.equals(0)) throw new BusinessException(ErrorCode.PARAMS_ERROR);
         List<Integer> commentIdList = commentService.selectIdsByInfoId(id);
-        Integer mysqlFlag = infoService.deleteInfoWithComments(id).get();
-        Integer esFlag = esService.delete(String.valueOf(id), commentIdList).get();
-        flagService.addFlag(new Flag(id, mysqlFlag, 1, esFlag, 0, 4));
-        String jsonUser = (String) request.getSession().getAttribute("user");
-        User user = JSON.parseObject(jsonUser, User.class);
+        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(result -> result)//如果结果为true，重试，这里没有设置暂停策略，所以会一直重试结果为false
+                .withWaitStrategy(WaitStrategies.fixedWait(5L, TimeUnit.SECONDS))
+                .build();
+        Flag flag = new Flag();//这个对象是为了在lambda表达式中绕过对变量必须为final的要求
+        Callable<Boolean> retryTask = () -> {
+            flag.mysqlFlag = infoService.deleteInfoWithComments(id).get();
+            flag.esFlag = esService.delete(String.valueOf(id), commentIdList).get();
+            // 只要其中一个成功（非0），就返回false结束重试
+            return flag.mysqlFlag == 0 && flag.esFlag == 0;
+        };
+        User user;
+        try {
+            //如果MySQL和ES都失败，那么重新操作，直到其中一个成功，不需要全部都成功，因为采用的是弱一致方案，有定时任务补偿
+            retryer.call(retryTask);
+            flagService.addFlag(new Flag(id, flag.mysqlFlag, 1, flag.esFlag, 0, 4));
+            String jsonUser = (String) request.getSession().getAttribute("user");
+            user = JSON.parseObject(jsonUser, User.class);
+        } catch (RetryException e) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
         return "redirect:/user/" + user.getUserId();
     }
 
@@ -285,7 +402,7 @@ public class APIController {
     @GetMapping("/recommendPosts")
     public String getRecommendPosts(HttpSession session) {
         String jsonUser = (String) session.getAttribute("user");
-        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR,"用户信息不存在");
+        if (jsonUser == null || jsonUser.isEmpty()) throw new BusinessException(ErrorCode.NULL_ERROR, "用户信息不存在");
         User user = JSON.parseObject(jsonUser, User.class);
         //调用定时任务返回推荐帖子数据，转成json给前端
         List<InfoWithCommentsDTO> recommendPosts = recommendPostsTask.getRecommendPosts(user.getUserId());
