257d9696de1b948df29df7678d6241941c34a850
15zed <kh1518879490@163.com>
2024-08-16 16:18:24 +0800

    delte part files

--- src/main/java/com/hgc/school/Constants/OperationResultConstant.java
+++
@@ -1,9 +0,0 @@
-package com.hgc.school.Constants;
-
-/**
- * 操作结果常量（ES，MySQL，Redis）
- */
-public class OperationResultConstant {
-    public static final int SUCCESS = 1;
-    public static final int FAILURE = 0;
-}
